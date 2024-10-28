b69db429657539f33574f82ddaed83af37348bf3
Tristanoss <tristan2003devos@gmail.com>
2024-10-28 16:13:04 +0100

    new:ex2.5-events

--- exercises/2.5/src/components/App.tsx
+++ exercises/2.5/src/components/App.tsx
@@ -16,7 +16,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <ClickCounter title='Click Counter' message='You are a master in the art of clicking !'/>
+        <ClickCounter 
+          title='Click Counter' 
+          message='You are a master in the art of clicking !'
+          hoverMessage='Please click on me now !'
+
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- exercises/2.5/src/components/ClickCounter.tsx
+++ exercises/2.5/src/components/ClickCounter.tsx
@@ -3,15 +3,23 @@ import { useState } from "react";
 interface ClickCounterProps {
     title : string;
     message : string;
+    hoverMessage : string;
 }
 
-const ClickCounter = ({title, message}: ClickCounterProps) => {
+const ClickCounter = ({title, message, hoverMessage}: ClickCounterProps) => {
   const [count, setCount] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div>
         <h2>{title}</h2>
-      <button onClick={() => setCount(count + 1)}>
+        {isHovered && <p>{hoverMessage}</p>}
+        <button 
+            onClick={() => setCount(count + 1)}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+
         Clicked {count} times
       </button>
       {count >= 10 && <p>{message}</p>}
