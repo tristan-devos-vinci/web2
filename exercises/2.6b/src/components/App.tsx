cce63adf6fd7f58ed90d49d14c711e2f9ae6d6af
Tristanoss <tristan2003devos@gmail.com>
2024-10-28 16:44:53 +0100

    new: ex2.6b

---
+++ exercises/2.6b/src/components/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ColorBox from './ColorBox';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ColorBox />
+    </div>
+  );
+};
+
+export default App;
